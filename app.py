568f50894f0997993135620464dfa8d4bc55190f
Kavin Saravanan <kavinsaravan@gmail.com>
2025-06-26 12:37:13 -0700

    added new files

--- app.py
+++ app.py
@@ -82,5 +82,7 @@ def create_entry():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+   #app.run(debug=True, port=5000)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
 
